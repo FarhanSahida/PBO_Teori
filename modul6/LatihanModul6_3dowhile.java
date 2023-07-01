781d4257eca6cd874525297361396515c83334da
FarhanSahida <farhansahida1101@gmail.com>
2023-07-01 19:56:48 +0700

    .

---
+++ modul6/LatihanModul6_3dowhile.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul6;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LatihanModul6_3dowhile {
+    public static void main(String args[]){
+            int r = 0;
+            do {
+             System.out.println("Farhan Sahida");
+             r++;
+             } while (r<100);
+            }  
+}
---
+++ modul6/LatihanModul6_3whileloop.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul6;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class LatihanModul6_3whileloop {
+    public static void main(String args[]){
+            int r=0;
+            while (r<100){
+             System.out.println("Melida Sari");
+             r++;
+            }
+           }
+}
